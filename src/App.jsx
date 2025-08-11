1f92e8e8eab458897cf3c59889e50ee38bd477be
goentvy <entvyv@gmail.com>
2025-08-11 14:54:51 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout'
+import Login from './components/Login'
+import Home from './components/Home'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>과제방</h1>
+      <BrowserRouter>
+        <Layout />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
