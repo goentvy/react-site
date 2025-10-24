c302301551580c746caf5d903a4446a5b7f7653a
goentvy <entvyv@gmail.com>
2025-10-24 16:05:14 +0900

    axios baseURL change

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'http://localhost:8090/api', // Spring Boot API 주소
+  baseURL: 'https://entvy-blog.onrender.com/api', // Spring Boot API 주소
   headers: {
     'Content-Type': 'application/json',
   },
